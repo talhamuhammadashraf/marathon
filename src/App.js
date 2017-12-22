d2d72de098b26050768af820ba8e7fe5789c6b5a
talhamuhammadashraf <talhamuhammadashraf@gmail.com>
2017-12-22 21:12:55 +0500

    at the end of the day

--- src/App.js
+++ src/App.js
@@ -1,29 +1,140 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DatePicker from 'material-ui/DatePicker';
 import axios from "axios"
-class App extends Component {
-  componentDidMount(){
-// axios.post("https://patientnode.herokuapp.com/CREATEPATIENT",{name:"any"}).then((data)=>{
-// console.log(data)
-axios.get('https://patientnode.herokuapp.com/CREATEPATIENT').then((data)=>{
-  console.log(data)
-})
-// })
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+
+class Comp extends Component {
+  componentDidMount() {
+    // axios.get("https://developer.schiphol.nl/apis/flight-api/flights","736271b6").then((dta)=>{console.log(dta)})
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // {/* <div style={{borderColor:"black"}}>{this.props.day.toDateString()}</div> */}
+      <div>
+
+        <input value={this.props.day.toDateString()} />
       </div>
-    );
+    )
+  }
+}
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      day: new Date()
+    }
+  }
+  componentDidUpdate() {
+    var date = this.state.day.toLocaleDateString();
+    date = date.split("/")
+    var date1 = date[2] + "-" + date[0] + "-" + date[1]
+    console.log("date1", date1)
+
+    axios.get(
+      "https://api.schiphol.nl/public-flights/flights?app_id=736271b6&app_key=92b67b61a7e5ed55211ef7584a05d618&scheduledate=" + date1 + "&includedelays=false&page=0&sort=%2Bscheduletime",
+      {
+        headers: { Accept: 'application/json', ResourceVersion: 'v3' }
+      }
+    )
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          data: data
+        })
+      })
+  }
+
+  componentDidMount() {
+    var date = this.state.day.toLocaleDateString();
+    date = date.split("/")
+    var date1 = date[2] + "-" + date[0] + "-" + date[1]
+    console.log("date1", date1)
+
+    axios.get(
+      "https://api.schiphol.nl/public-flights/flights?app_id=736271b6&app_key=92b67b61a7e5ed55211ef7584a05d618&scheduledate=" + date1 + "&flight" +this.state.flight,
+      {
+        headers: { Accept: 'application/json', ResourceVersion: 'v3' }
+      }
+    )
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          data: data
+        })
+      })
+  }
+
+
+  componentWillReceiveProps(props, state) {
+    console.log("state", state)
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        <DayPickerInput onDayChange={(day) => { this.setState({ day: day }) }} />
+        <form>
+          <input onChange={(event) => { this.setState({ flight: event.target.value }) }} />
+          <button onClick={() => {
+            var date = this.state.day.toLocaleDateString();
+            date = date.split("/")
+            var date1 = date[2] + "-" + date[0] + "-" + date[1]
+
+            axios.get(
+              "https://api.schiphol.nl/public-flights/flights?app_id=736271b6&app_key=92b67b61a7e5ed55211ef7584a05d618&scheduledate=" + date1+ "&flight=" + this.state.flight,
+              {
+                headers: { Accept: 'application/json', ResourceVersion: 'v3' }
+              }
+            )
+              .then((data) => {
+                console.log(data)
+                this.setState({
+                  data: data
+                })
+              })
+
+          }}>submit</button>
+        </form>
+        <div>
+
+        </div>
+
+
+        {this.state.data ?
+          this.state.data.data.flights.map((DATA, INDEX) => {
+            console.log(DATA.flightName)
+
+
+            return <table key={INDEX}><tr style={{}}>
+
+              <td style={{ borderBottom: "1px solid black", borderLeft: "1px solid black", height: 15, borderRadius: 0, width: 180, margin: 5, padding: 10 }}>{DATA.flightName}</td>
+              <td style={{ borderBottom: "1px solid black", borderLeft: "1px solid black", height: 15, borderRadius: 0, width: 180, margin: 5, padding: 10 }}>Date :{DATA.scheduleDate}{"\n"}Time:{DATA.scheduleTime}</td>
+              <td style={{ borderBottom: "1px solid black", borderLeft: "1px solid black", height: 15, borderRadius: 0, width: 180, margin: 5, padding: 10 }}>Date :{new Date(DATA.estimatedLandingTime).toLocaleDateString()}{"\n"}Time :{new Date(DATA.estimatedLandingTime).toLocaleTimeString()}</td>
+            </tr>
+            </table>
+            // {DATA.flightName}
+
+          }) : "..."}
+
+      </div>
+    )
   }
 }
 
+
+
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import moment from "moment"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// var m=moment(new Date().toLocaleDateString,"YYYY/MM/DD")
+// console.log(m)
+ReactDOM.render(
+<MuiThemeProvider>
+<App />
+</MuiThemeProvider>
+, document.getElementById('root'));
 registerServiceWorker();
